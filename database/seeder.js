da4e3170b9ab4223bf18d2f40cd0c1b173e29f19
Daniel Flores <danielflores125@gmail.com>
2020-09-24 12:13:10 -0700

    Cleaned up extraneouse lines code

--- database/seeder.js
+++ database/seeder.js
@@ -9,23 +9,15 @@ var randomItem = (array) =>{
 
 var bedrooms = [2,3,4]
 
-
 var baths = [1,2,3]
-
 
 var sqfootage = [1200,1622,1432,1890,2102,2219,1352,1029,1609,1634,1478,2010,1589]
 
-
-
 var district = ['Marina', 'Mission', 'SOMA', 'NOPA', 'Sunset', 'Twin Peaks', 'Russian Hill', 'Dogpatch']
-
-
 
 var address = ['420 Precita Ave', '430 Lily St', '33 Shields St', '354 Eddy St', '36 Cragmont Ave', '216 Eddy St', '1405 Gough St', '189 Majestic Ave', '209 Blythedale Ave', '1366 33rd Ave', '1650 Page St #104', '254 Bright St', '2701 Green St', '184 Linda St', '1366 33rd Ave', '794 Mcallister St', '66 Ceres St', '766 Sutter St']
 
-
 var price = [1302934, 1349009,1446011, 1243011, 1013014,1526125, 1245366, 1245039, 2049509, 1237189, 14552149, 1583255, 1495873, 1920193]
-
 
 var generateRandomListing = () => {
   var listing = {}
